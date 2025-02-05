7a15631337cd330b2fd5a76edecd19723c6dec74
IoTSabaAzizzadeh <iot.sabaazizzadeh@gmail.com>
2025-02-05 09:44:59 +0330

    second commit

--- app.js
+++ app.js
@@ -1,11 +1,49 @@
-async function getQrCode() {
+const buttonEL = document.querySelector('.GenerateButton')
+
+buttonEL.addEventListener('click' , () => {
+    getQR()
+})
+
+
+
+const clearEL = document.querySelector('.clearButton')
+
+clearEL.addEventListener('click' , () => {
+    clearText()
+})
+
+async function getQR(){
+    
+    
     try{
-        const data = URL("https://goqr.me/api/")
+        const inputEL = document.querySelector('input').value;
+        if (inputEL.trim() === ""){
+            alert ("Please enter text or a link")
+            return;
+        }
+      
+        const response = await fetch('https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example');
+        const blob = await response.blob();
+        const imgURL = URL.createObjectURL(blob);
 
 
+
+        const img = document.createElement("img")
+        img.src = imgURL
+        
+
+        const divEL = document.querySelector('.QR');
+        divEL.appendChild(img)
     }
-    catch(err){
-        console.log("error");
+    catch(error){
+        console.error("Error fetching image:", error);
         
     }
+
+
+
+}
+
+function clearText(){
+    return (document.querySelector('inputEL').reset());
 }
